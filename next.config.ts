e89ee7a6a616794c57b4378e93cd656c9b647b6f
yqianjiang <y.jiang.psy@gmail.com>
2025-04-07 00:28:41 +0800

    使用 github pages 部署

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
+  basePath: '/finance-tracker',
 };
 
 export default nextConfig;
